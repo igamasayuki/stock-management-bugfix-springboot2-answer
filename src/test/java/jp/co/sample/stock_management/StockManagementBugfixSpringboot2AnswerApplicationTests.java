112963ad5a6b3a6440d6b30aae4ac7bda70f93e2
Masayuki IGA <igamasayuki@gmail.com>
2018-11-26 11:59:43 +0900

    テストサンプル追加

--- src/test/java/jp/co/sample/stock_management/StockManagementBugfixSpringboot2AnswerApplicationTests.java
+++ src/test/java/jp/co/sample/stock_management/StockManagementBugfixSpringboot2AnswerApplicationTests.java
@@ -2,15 +2,24 @@ package jp.co.sample.stock_management;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import jp.co.sample.stock_management.repository.BookRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StockManagementBugfixSpringboot2AnswerApplicationTests {
 
+	@Autowired
+	private BookRepository bookRepository;
+	
 	@Test
-	public void contextLoads() {
+	public void testFindAll() {
+		
+		bookRepository.findAll().forEach(System.out::println);
+		
 	}
 
 }
